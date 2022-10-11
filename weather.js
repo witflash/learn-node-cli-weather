9f989f249af69061d305a89aaa56cce192f3dd27
Andrii Mambyk <a.mambyk@weplay.tv>
2022-10-11 22:34:29 +0300

    handle argv

--- weather.js
+++ weather.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+    const args = getArgs(process.argv);
+    console.log(args);
+
+    if (args.h) {
+        // Help view
+    }
+
+    if (args.s) {
+        // Save a city
+    }
+
+    if (args.t) {
+        // Save token
+    }
+
+    // Show weathercast
+}
+
+initCLI();
