172fba3acf2cefc70b2a9398f4368304f80010d3
Andrii Mambyk <a.mambyk@weplay.tv>
2022-10-11 23:23:50 +0300

    add log helpers

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = error => {
+    console.log(chalk.bgRed(' ERROR') + ` ${error}`);
+}
+
+const printSuccess = message => {
+    console.log(chalk.bgGreen(' SUCCESS') + ` ${message}`)
+}
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan(' HELP ')}
+        Without params: show weather
+        -s [CITY] to set the city
+        -h to view help
+        -t [API_KEY] to save the token
+        `
+    );
+}
+
+export { printError, printSuccess, printHelp }
