b610fb638b89495b27380c3ce1a62f60f0765b63
Andrii Mambyk <a.mambyk@weplay.tv>
2022-10-11 23:37:28 +0300

    add base storage service

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
 import { printHelp } from './services/log.service.js'
+import { saveKeyValue } from './services/storage.service.js'
 
 const initCLI = () => {
     const args = getArgs(process.argv);
@@ -15,7 +16,7 @@ const initCLI = () => {
     }
 
     if (args.t) {
-        // Save token
+        saveKeyValue('token', args.t)
     }
 
     // Show weathercast
